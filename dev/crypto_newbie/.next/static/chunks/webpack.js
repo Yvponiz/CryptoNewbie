1a3df3e2c4983d3e3461ebcfd47336a215edbcc0
NikolasOuim <78503903+NikolasOuim@users.noreply.github.com>
2022-03-31 12:20:21 -0400

    correction bug dans la recherche

--- dev/crypto_newbie/.next/static/chunks/webpack.js
+++ dev/crypto_newbie/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0392932b012e82cf"; }
+/******/ 		__webpack_require__.h = function() { return "dc05e51c2fa5f448"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- dev/crypto_newbie/pages/api/searchCrypto.ts
+++ dev/crypto_newbie/pages/api/searchCrypto.ts
@@ -4,8 +4,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 
-export async function searchCrypto(crypto) {
-    let data = await CoinGeckoClient.coins.fetch(crypto, {});
+export async function searchCrypto(crypto:string) {
+    let data = await CoinGeckoClient.coins.fetch(crypto.toLowerCase(), {});
     return data.data
 }
 
