ba604f7433fc323647854ae9cd4917a58bc15efd
NikolasOuim <78503903+NikolasOuim@users.noreply.github.com>
2022-04-28 13:25:06 -0400

    algorithme presque terminé

--- dev/crypto_newbie/frontend/components/cryptoList.tsx
+++ dev/crypto_newbie/frontend/components/cryptoList.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Coin } from "../utils/coin";
 import handler from "../../pages/api/worstCrypto";
+import quickSort from "../utils/quickSort";
 
 export default function CryptoList() {
-    let arr = []
     const [handlerState, setHandlerState] = useState<Coin[]>([])
-    arr = handlerState
-    useEffect(() => { // Fetch les data coté client
+
+    useEffect(() => {
         fetch('/api/coinList') // Appelle la fonction exporté par défaut dans coinList
             .then((res) => res.json())
             .then((data) => setHandlerState(data as Coin[]))
@@ -15,17 +15,20 @@ export default function CryptoList() {
 
     //arr = handlerState.sort(function(a, b) {return a.market_data.current_price.cad - b.market_data.current_price.cad})
     //const filterByName = () => {
-        //handlerState.sort(function(a,b) {return a.name.localeCompare(b.name)})
+      //  handlerState.sort(function(a,b) {return a.name.localeCompare(b.name)})
      //   arr = handlerState.sort(function(a,b) {return a.name.localeCompare(b.name)}) //pour comparer les noms
      //   console.log("bonjour")
+        console.log(handlerState)
     //}
 
-    const filterByName = (() => {
-        arr = handlerState.slice(0,25).sort(function(a,b) {return a.name.localeCompare(b.name)})
-        console.log(arr)
-        console.log("bonjour")
-        //window.location.reload(false);
-        //document.getElementById('listCrypto').innerHTML = document.getElementById('listCrypto').innerHTML
+    const filterByName = (async () => {
+        //ces trois lignes doivent être conservé pour faire des tests
+        //setHandlerState(handlerState.slice(0,25).sort(function(a,b) {return a.name.localeCompare(b.name)}))
+        //console.log(handlerState)
+        //document.getElementById('listCrypto').remove
+        let newList = await quickSort(handlerState, "name")
+        setHandlerState(newList)
+        console.log("bonsoir")
     })
    
 
@@ -41,7 +44,7 @@ export default function CryptoList() {
                 <p>24 heures</p>
             </div>
 
-            <div id="listCrypto">{arr.slice(0, 25)
+            <div id="listCrypto">{handlerState.slice(0, 25)
                 .map(({ id, name, symbol, market_data: { current_price, market_cap, market_cap_rank, price_change_percentage_24h }, image: { small }}) =>
                     <a href='' className='index-coin' key={id}>
                         <li>{market_cap_rank}</li>
--- dev/crypto_newbie/frontend/utils/quickSort.ts
+++ dev/crypto_newbie/frontend/utils/quickSort.ts
@@ -1,15 +1,17 @@
-export default function quickSort(array, element){
+export default async function quickSort(listCrypto, element){
     //console.log(array.slice(0,5).map(coin => coin.element));
-
-    let map = new Map()
-
-    map = array.slice(0,25).map()
-
-    for(let i = 0; i < map.size; i++){
-        for(let j = 0; j < map.size - i - 1; j++){
-             if(map.get){
-                 
-             }
+    let list = listCrypto.slice(0,25)
+    for(let i = 0; i < list.length; i++){
+        for(let j = i - 1; j >= 0; j--){
+            if (element === "name"){
+                if (list[j + 1].name < (list[j].name)){
+                    [list[j + 1], list[j]] = [list[j], list[j + 1]]
+                } 
+            }
+             //if(listCrypto[i + 1] < listCrypto[j]){
+                //[arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
         }
     }
+        console.log(list)
+        return list
 }
